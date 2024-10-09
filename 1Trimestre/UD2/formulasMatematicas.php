2b0e3ad735ab5afaa8c59bbc464d577663292d99
PhilippeInCode <felipechacon.montero@alumnos.cei.es>
2024-10-09 14:44:00 +0200

    Add new PHP practices

---
+++ 1Trimestre/UD2/formulasMatematicas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulas</title>
+</head>
+<body>
+    <?php
+    $pi = M_PI;
+    $radio = 5;
+    $superficie = $pi * ($radio**2);
+    $longitud = 2 * $pi * $radio;
+    $volumen = 4/3 * $pi * ($radio**3);
+    
+    echo "<h1>La superficie es: $superficie</h1>
+    <h2>La longitud es: $longitud</h2>
+    <h3>El volumen: $volumen</h3>"
+
+    ?>  
+</body>
+</html>
